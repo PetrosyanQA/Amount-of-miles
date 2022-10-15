200042589fc501823eb1e055faac25d2407cee99
Petrosyan Test <job-dream99@yandex.ru>
2022-10-16 00:35:43 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Итоговый миль за пакупки");
+        int amount = 1000;
+         boolean b = amount >= - 20;
+
+        int bonus;
+        if (b) {
+            bonus = (amount/20);
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
+    }
+
+}
