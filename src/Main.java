21e40c3676f96df5eb9fd2338b426562afb6bbcf
Petrosyan Test <job-dream99@yandex.ru>
2022-11-24 18:55:08 +0400

    add commit

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Итоговый миль за пакупки");
-        int amount = 1000;
+        int ticketCost = 1000;
 
-        int bonus;
-        bonus = (amount / 20);
-        System.out.println(bonus);
+        int mileBonus;
+        mileBonus = (ticketCost / 20);
+        System.out.println(mileBonus);
     }
 
 }
